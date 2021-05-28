37623d811f6d29f2fe52b11dd593a47b4c322b29
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-05-28 11:16:37 -0400

    sequelize

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+// import the Sequelize constructor from the library
+const Sequelize = require('sequelize');
+
+// create connection to our database, pass in your MySQL information for username and password
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, { //importing the base Sequelize class and using it to create a new connection to the database
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+});
+
+//To deploy the application, you had to set the server's port configuration
+const PORT = process.env.PORT || 3001; //process.env.port is because Heroku reserve the port 3001 for us and will provide a port dynamically
+
+
+
+module.exports = sequelize;
---
+++ models/index.js
@@ -0,0 +1,3 @@
+const User = require('./User');  //   importing the User model and exporting an object with it as a property
+
+module.exports = { User };
